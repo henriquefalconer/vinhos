3ca7d42d494481abcff3a5368bb67d56552a4855
Henrique <henrique@falconer.com.br>
2021-08-19 16:24:45 -0300

    fix: remove debug areas

--- src/screens/GraphScreen/index.tsx
+++ src/screens/GraphScreen/index.tsx
@@ -7,26 +7,10 @@ import Graph from '../../components/Graph';
 import * as S from './styles';
 
 const GraphScreen: React.FC = () => {
-  const { wineArea, foodArea, interArea, unionArea } = useHarmonization();
+  const { interArea, unionArea } = useHarmonization();
 
   return (
     <S.Container>
-      <S.Text>
-        Área vinho: <S.Bold>{wineArea.toFixed(2)}</S.Bold>
-      </S.Text>
-      <S.Space size={20} />
-      <S.Text>
-        Área alimento: <S.Bold>{foodArea.toFixed(2)}</S.Bold>
-      </S.Text>
-      <S.Space size={20} />
-      <S.Text>
-        Área intersecção: <S.Bold>{interArea.toFixed(2)}</S.Bold>
-      </S.Text>
-      <S.Space size={20} />
-      <S.Text>
-        Área combinada: <S.Bold>{unionArea.toFixed(2)}</S.Bold>
-      </S.Text>
-      <S.Space size={34} />
       <Graph />
       <S.Space size={34} />
       <S.Text>
