da89d1d69acf883c88a0adfa12863a7a75cb4c01
Henrique <henrique@falconer.com.br>
2021-08-25 00:32:41 -0300

    fix: refactor polygon

--- src/utils/polygon.ts
+++ src/utils/polygon.ts
@@ -1,16 +1,18 @@
-import { getAxisEnd } from './radar';
-
 const geometric = require('geometric');
 
 export const getPolygonArea = (p: geometric.Polygon) =>
   geometric.polygonArea(p) / 319.5;
 
+const getPolygonEnd = (
+  [[x1, y1], [x2, y2]]: geometric.Line,
+  multiplier: number
+): geometric.Point => [
+  x1 + (x2 - x1) * multiplier,
+  y1 + (y2 - y1) * multiplier,
+];
+
 export const buildPolygon = (
   points: number[],
-  angles: number[],
-  center: number,
-  axisSize: number
+  axes: geometric.Line[]
 ): geometric.Polygon =>
-  points.map((p, i, _, a = angles[i]) =>
-    getAxisEnd(a, axisSize, center, 0, Math.max(p, 1e-14) / 10)
-  );
+  points.map((p, i) => getPolygonEnd(axes[i], Math.max(p, 1e-14) / 10));
