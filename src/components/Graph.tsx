a922463314ec81e7ee8a6ad54980df113f427412
Henrique <henrique@falconer.com.br>
2021-08-24 22:46:44 -0300

    fix: simplify Graph & delete Knob

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
-import {
-  Svg,
-  Circle,
-  Line,
-  Path,
-  Defs,
-  Stop,
-  LinearGradient,
-} from 'react-native-svg';
+import { Svg, Circle, Line, Path } from 'react-native-svg';
 
 import {
   wineAngles,
@@ -19,17 +11,8 @@ import {
 
 import { useHarmonization } from '../hooks/useHarmonization';
 
-import Knob from './Knob';
-
 const Graph: React.FC = () => {
-  const {
-    wineScores,
-    foodScores,
-    setWineScores,
-    setFoodScores,
-    setGraphLayout,
-    polygonData,
-  } = useHarmonization();
+  const { setGraphLayout, polygonData } = useHarmonization();
 
   return (
     <View
@@ -49,30 +32,7 @@ const Graph: React.FC = () => {
                 strokeWidth="2"
               />
             ))}
-            {wineAngles.map(
-              (
-                angle,
-                i,
-                _,
-                [x, y] = getAxisEnd(
-                  angle,
-                  polygonData.axisSize,
-                  polygonData.graphCenter,
-                  15
-                )
-              ) => (
-                <Line
-                  key={i}
-                  x1="50%"
-                  y1="50%"
-                  x2={`${x}`}
-                  y2={`${y}`}
-                  stroke="white"
-                  strokeWidth="3.75"
-                />
-              )
-            )}
-            {foodAngles.map(
+            {[...wineAngles, ...foodAngles].map(
               (
                 angle,
                 i,
@@ -95,62 +55,17 @@ const Graph: React.FC = () => {
                 />
               )
             )}
-            <Defs>
-              <LinearGradient id="grad1" x1="0%" y1="0%" x2="0%" y2="100%">
-                <Stop offset="0%" stopColor="#5900ff" stopOpacity="0.6" />
-                <Stop offset="100%" stopColor="#66e200" stopOpacity="0.6" />
-              </LinearGradient>
-              <LinearGradient id="grad2" x1="0%" y1="0%" x2="100%" y2="0%">
-                <Stop
-                  offset="0%"
-                  stopColor="rgb(255,255,0)"
-                  stopOpacity="0.6"
-                />
-                <Stop
-                  offset="100%"
-                  stopColor="rgb(255,0,0)"
-                  stopOpacity="0.6"
-                />
-              </LinearGradient>
-            </Defs>
             <Path
               d={generatePolygonPath(polygonData.winePolygon)}
-              fill="url(#grad1)"
+              fill="#5900ff9b"
               stroke="white"
             />
             <Path
               d={generatePolygonPath(polygonData.foodPolygon)}
-              fill="url(#grad2)"
+              fill="#ffff0099"
               stroke="white"
             />
           </Svg>
-          {wineScores.map((score, i, _, a = wineAngles[i]) => (
-            <Knob
-              key={i}
-              score={score}
-              onChange={(v) =>
-                setWineScores((scores) => {
-                  scores[i] = v;
-                  return [...scores];
-                })
-              }
-              angle={a}
-            />
-          ))}
-
-          {foodScores.map((score, i, _, a = foodAngles[i]) => (
-            <Knob
-              key={i}
-              score={score}
-              onChange={(v) =>
-                setFoodScores((scores) => {
-                  scores[i] = v;
-                  return [...scores];
-                })
-              }
-              angle={a}
-            />
-          ))}
         </>
       )}
     </View>
