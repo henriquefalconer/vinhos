eb3259a036d9f706d00d1268e72653b1a7dbc47e
Henrique <henrique@falconer.com.br>
2021-08-24 22:47:03 -0300

    feat: add new wine angles

--- src/utils/radar.ts
+++ src/utils/radar.ts
@@ -2,7 +2,7 @@ import geometric from 'geometric';
 
 const covertAngle = (a: number) => ((a - 90) * Math.PI) / 180;
 
-export const wineAngles = [0, 120, 240].map(covertAngle);
+export const wineAngles = [-5, 5, 115, 125, 235, 245].map(covertAngle);
 export const foodAngles = [50, 70, 170, 190, 290, 310].map(covertAngle);
 
 export const generatePolygonPath = (points: geometric.Polygon) =>
