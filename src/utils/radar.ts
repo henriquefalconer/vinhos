a1e40e0230191990d09b63e0b014274857c2c890
Henrique <henrique@falconer.com.br>
2021-08-25 00:52:13 -0300

    fix: refactor offA & offB

--- src/utils/radar.ts
+++ src/utils/radar.ts
@@ -25,17 +25,17 @@ const getParallelAxes = (line: geometric.Line, offset = 0) => {
     b = line[0][1],
     c = line[1][0],
     d = line[1][1],
-    offX = (c - a) * offset,
-    offY = (d - b) * offset,
+    offA = a + (c - a) * offset,
+    offB = b + (d - b) * offset,
     size = Math.sqrt((c - a) ** 2 + (d - b) ** 2),
     nx = ((b - d) / size) * PARALLEL_DISTANCE,
     ny = ((c - a) / size) * PARALLEL_DISTANCE,
     p1 = [
-      [a - nx + offX, b - ny + offY],
+      [offA - nx, offB - ny],
       [c - nx, d - ny],
     ],
     p2 = [
-      [a + nx + offX, b + ny + offY],
+      [offA + nx, offB + ny],
       [c + nx, d + ny],
     ];
 
